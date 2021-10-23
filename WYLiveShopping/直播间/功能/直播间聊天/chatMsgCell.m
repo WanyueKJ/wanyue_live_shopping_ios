4d6638471d44b1a0fa80f02ad82ef1ff88f27a20
张琰 <apple@zhangyan.local>
2021-10-23 17:48:58 +0800

    wu

--- WYLiveShopping/直播间/功能/直播间聊天/chatMsgCell.m
+++ WYLiveShopping/直播间/功能/直播间聊天/chatMsgCell.m
@@ -17,8 +17,6 @@
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 #pragma mark - TODO
 -(void)setModel:(chatModel *)model{
